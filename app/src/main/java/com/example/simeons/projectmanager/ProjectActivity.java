f43bc91a4a798b5eb638787ab5d0c7441de7dccd
Simeon Sidey <SimeonS@wolf-consultancy.co.uk>
2016-10-26 14:16:03 +0100

    Up to Project manager activity

--- app/src/main/java/com/example/simeons/projectmanager/ProjectActivity.java
+++ app/src/main/java/com/example/simeons/projectmanager/ProjectActivity.java
@@ -1,6 +1,7 @@
 package com.example.simeons.projectmanager;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,7 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ProjectActivity extends ListActivity {
+public class ProjectActivity extends Activity {
     ListView listView ;
 
     @Override
@@ -20,7 +21,7 @@ public class ProjectActivity extends ListActivity {
         listView = (ListView) findViewById(R.id.list);
 
         // Defined Array values to show in ListView
-        String[] values = new String[] { "Android List View",
+        String[] values = new String[]{"Android List View",
                 "Adapter implementation",
                 "Simple List View In Android",
                 "Create List View Android",
@@ -51,19 +52,28 @@ public class ProjectActivity extends ListActivity {
                                     int position, long id) {
 
                 // ListView Clicked item index
-                int itemPosition     = position;
+                int itemPosition = position;
 
                 // ListView Clicked item value
-                String  itemValue    = (String) listView.getItemAtPosition(position);
+                String itemValue = (String) listView.getItemAtPosition(position);
 
                 // Show Alert
                 Toast.makeText(getApplicationContext(),
-                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
                         .show();
 
             }
 
         });
+
+
+    }
+
+    public void addNewProject(View view) {
+
+        Intent intent = new Intent(ProjectActivity.this, ProjectManagerActivity.class);
+        startActivity(intent);
+
     }
 
 }
