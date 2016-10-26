8b17cb033a5a53c6938a31b92bbeefb78aa0f4fe
Simeon Sidey <SimeonS@wolf-consultancy.co.uk>
2016-10-26 12:12:20 +0100

    Deleted duplicate folders

--- app/src/main/java/com/example/simeons/projectmanager/LoginActivity.java
+++ app/src/main/java/com/example/simeons/projectmanager/LoginActivity.java
@@ -3,21 +3,19 @@ package com.example.simeons.projectmanager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -49,7 +47,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "SS", "SS40"
+            "virmons@hotmail.co.uk", "SS40L0ngEnoughNow"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
